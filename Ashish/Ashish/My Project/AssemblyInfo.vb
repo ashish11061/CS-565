ec022e54767e898521f431a324f492280d80b987
ashish11061@gmail.com <ashish11061@gmail.com>
2015-12-05 16:16:10 -0800

    fdsa

---
+++ Ashish/Ashish/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Ashish")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Ashish")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("37eb7015-cf13-4af4-9053-3b30f5c749ae")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
